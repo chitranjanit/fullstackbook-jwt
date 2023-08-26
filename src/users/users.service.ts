5f36163501b5c2c992a6a8c4282c26d14d9eed17
chitranjan mukhiya <chitranjan.it@gmail.com>
2023-08-26 09:01:23 +0530

    auth token generate

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
+// This should be a real class/interface representing a user entriy
+export type User = any;
+
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private readonly users = [
+    {
+      userId: 1,
+      username: 'chitra',
+      password: 'chitranjan',
+    },
+    {
+      userId: 2,
+      username: 'sanjeep',
+      password: 'sanjeep',
+    },
+  ];
+
+  async findOne(username: string): Promise<User | undefined> {
+    return this.users.find((user) => user.username === username);
+  }
+}
